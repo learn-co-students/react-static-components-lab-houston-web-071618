1176b204fa706c89bf0ad831ec9757f1a955043e
Jeannie Kim <SJ.Jeannie.Kim@gmail.com>
2018-09-19 15:54:26 -0500

    Done.

--- src/FordQuoteComponent.js
+++ src/FordQuoteComponent.js
@@ -4,10 +4,17 @@ export default class FordQuoteComponent extends Component {
   render() {
     return (
       <div className="bar" id="ford">
-        <i><p>&quot;If I had asked people what they wanted, they would have said faster horses&quot;</p></i>
+        <i>
+          <p>
+            &quot;If I had asked people what they wanted, they would have said
+            faster horses&quot;
+          </p>
+        </i>
         <br />
         &#9;-Henry Ford
       </div>
     );
   }
 }
+
+// export default FordQuoteComponent;
